93b2f8f51ab50f7c31c9bba5254039237c9b4c52
tnguyen-win <129979622+tnguyen-win@users.noreply.github.com>
2023-04-19 18:10:11 -0700

    Lab 2 - concentration

--- src/indexStartV2.js
+++ src/indexStartV2.js
@@ -1,38 +1,159 @@
-// Create a class called Concentration.
-{
-    /*
-        Add a constructor.  In the body of the constructor
-        -   Create instance variables to replace the global variables
-        -   Bind the class to each of the following methods
-        -       this.showMatches = this.showMatches.bind(this);
-        -       this.enableAllRemainingCards = this.enableAllCards.bind(this);
-        -       this.enableAllRemainingCards = this.enableAllRemainingCards.bind(this);
-        -       this.checkCards = this.checkCards.bind(this);
-        -       this.disableAllCards = this.disableAllCards.bind(this);
-        -       this.isMatch = this.isMatch.bind(this);     
-        -   All of the functionality of init will happen in the constructor ... call init.
-    */
-    /*
-        Convert each function to a method.  
-        -   Move it inside the class.
-        -   Remove the keyword function
-        -   Add this. in front of every variable and method
-        
-        THREE OF THE METHODS CHANGE A LITTLE
-        -   handleClick will now have a parameter, index
-            -   remove the declaration / assignment of the local var index
-        -   enableAllCards (and enableAllRemainingCards) have to pass the index to handleClick
-            -   the line of code that calls bind must now pass both this and an index
-            -   before: cards[i].onclick = this.handleClick.bind(this);
-            -   should be: cards[i].onclick = this.handleClick.bind(this, i);
-    */
+/* jshint esversion: 6 */
+
+class Concentration {
+	constructor() {
+		this.imagePath = "Cards/";
+		this.images = Array(19).fill(null);
+		this.firstPick = -1;
+		this.secondPick = -1;
+		this.matches = 0;
+		this.tries = 0;
+
+		this.showMatches = this.showMatches.bind(this);
+		this.enableAllCards = this.enableAllCards.bind(this);
+		this.enableAllRemainingCards = this.enableAllRemainingCards.bind(this);
+		this.checkCards = this.checkCards.bind(this);
+		this.disableAllCards = this.disableAllCards.bind(this);
+		this.isMatch = this.isMatch.bind(this);
+
+		this.init();
+	}
+
+	init() {
+		this.fillImages();
+		this.shuffleImages();
+		this.showMatches();
+		this.enableAllCards();
+		this.showAllBacks();
+
+		// for (let i in this.images) {
+		// 	let cardImage = this.imagePath + this.images[i];
+		// 	let card = document.getElementById(i);
+
+		// 	card.style.backgroundImage = `url("${cardImage}")`;
+		// };
+	}
+
+	showMatches() {
+		document.getElementById("status").innerText = `Matches: ${this.matches} | Tries: ${this.tries}`;
+	}
+
+	fillImages() {
+		const values = ["a", "k", "q", "j", "t", "9", "8", "7", "6", "5"];
+		const suits = ["h", "s"];
+		let index = 0;
+
+		for (let n1 of values) {
+			for (let n2 of suits) {
+				this.images[index] = `card${n1}${n2}.jpg`;
+				index++;
+			}
+		}
+	}
+
+	shuffleImages() {
+		for (let i of this.images) {
+			let r = Math.floor(Math.random() * this.images.length);
+			let temp = i;
+
+			i = this.images[r];
+			this.images[r] = temp;
+		}
+	}
+
+	enableAllCards() {
+		const cards = document.getElementsByName("card");
+
+		for (let c of cards) {
+			c.onclick = this.handleClick.bind(this, c.id);
+			c.style.cursor = "pointer";
+		}
+	}
+
+	enableAllRemainingCards() {
+		const cards = document.getElementsByName("card");
+
+		for (let c of cards) {
+			if (c.style.backgroundImage !== "none") {
+				c.onclick = this.handleClick.bind(this, c.id);
+				c.style.cursor = "pointer";
+			}
+		}
+	}
+
+	showBack(index) {
+		let backImage = `${this.imagePath}black_back.jpg`;
+		let card = document.getElementById(index);
+
+		card.style.backgroundImage = `url("${backImage}")`;
+	}
+
+	showAllBacks() {
+		const cards = document.getElementsByName("card");
+
+		for (let c of cards) this.showBack(c.id);
+	}
+
+	handleClick(index) {
+		let cardImage = `${this.imagePath}${this.images[index]}`;
+
+		document.getElementById(index).style.backgroundImage = `url("${cardImage}")`;
+
+		this.disableCard(index);
+
+		if (this.firstPick === -1) this.firstPick = index;
+		else {
+			this.secondPick = index;
+			this.disableAllCards();
+			setTimeout(this.checkCards, 2000);
+		}
+	}
+
+	disableCard(index) {
+		let card = document.getElementById(index);
+
+		card.onclick = () => { };
+		card.style.cursor = "none";
+	}
+
+	disableAllCards() {
+		const cards = document.getElementsByName("card");
+
+		for (let c of cards) this.disableCard(c.id);
+	}
+
+	checkCards() {
+		this.tries++;
+
+		if (this.isMatch()) {
+			this.matches++;
+			this.removeCard(this.firstPick);
+			this.removeCard(this.secondPick);
+			if (this.matches < 10) this.enableAllRemainingCards();
+		} else {
+			this.showBack(this.firstPick);
+			this.showBack(this.secondPick);
+			this.enableAllRemainingCards();
+		}
+
+		this.showMatches();
+
+		this.firstPick = this.secondPick = -1;
+	}
+
+	isMatch() {
+		if (this.images[this.firstPick].substr(4, 1) === this.images[this.secondPick].substr(4, 1)) return true;
+
+		return false;
+	}
+
+	removeCard(index) {
+		let card = document.getElementById(index);
+
+		card.style.backgroundImage = "none";
+	}
 }
 
-// create a variable called concentration
-// Add an event handler to the load event of the window. 
-// Use an anonymous function or an arrow function to
-// set the concentration variable to an instance of Concentration
+var concentration;
 
-
-
-
+window.onload = () => { concentration = new Concentration(); };
